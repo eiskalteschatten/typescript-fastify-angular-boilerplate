73b825f41679b450ac8d76061f694d5a28e2b494
Alex Seifert <alex@alexseifert.com>
2023-05-09 15:53:57 +0200

    Add the login component

--- packages/client/src/app/modules/auth/auth-routing.module.ts
+++ packages/client/src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
+
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AuthComponent
+    redirectTo: '/auth/login', pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
