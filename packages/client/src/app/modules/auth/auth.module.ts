5d43362431a80f4a9bc4dc591466750f1774ee1d
Alex Seifert <alex@alexseifert.com>
2023-05-09 15:54:29 +0200

    Add the register component

--- packages/client/src/app/modules/auth/auth.module.ts
+++ packages/client/src/app/modules/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
