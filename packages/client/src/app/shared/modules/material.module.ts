b961efb1319484c0375911013e067c15f1f7ea2c
Alex Seifert <alex@alexseifert.com>
2023-05-10 10:13:09 +0200

    Style the auth layout and login components

--- packages/client/src/app/shared/modules/material.module.ts
+++ packages/client/src/app/shared/modules/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 const sharedMaterialModules = [
   MatFormFieldModule,
   MatInputModule,
-  MatButtonModule
+  MatButtonModule,
+  MatCardModule
 ];
 
 @NgModule({
