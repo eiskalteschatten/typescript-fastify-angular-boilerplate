3517b3a0b4a3b1f20711b5d3be4877d4ffc9e8f8
Alex Seifert <alex@alexseifert.com>
2023-05-10 13:13:40 +0200

    Add a progress bar to the registration and login pages

--- packages/client/src/app/shared/modules/material.module.ts
+++ packages/client/src/app/shared/modules/material.module.ts
@@ -3,12 +3,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const sharedMaterialModules = [
   MatFormFieldModule,
   MatInputModule,
   MatButtonModule,
-  MatCardModule
+  MatCardModule,
+  MatProgressBarModule
 ];
 
 @NgModule({
